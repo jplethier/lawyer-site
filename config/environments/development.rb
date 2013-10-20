d5207efdf75a56c7a2bd5efecb41c87fbf0a1fc5
João Paulo Lethier <jplethier@gmail.com>
2013-10-19 21:46:08 -0300

    mailer configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,6 +22,13 @@ LawyerSite::Application.configure do
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,20 @@ LawyerSite::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default = { charset: "utf-8" }
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => :plain,
+    :domain => 'brunaxavier.com', #ENV['GMAIL_SMTP_USER'],
+    :user_name => 'brunarxavier@gmail.com', #ENV['GMAIL_SMTP_USER'],
+    :password => '485784672', #ENV['GMAIL_SMTP_PASSWORD'],
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
